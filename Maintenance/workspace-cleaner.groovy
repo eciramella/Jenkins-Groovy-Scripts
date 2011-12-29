e774ee7af800a02402c7437608f8d0a2eb138d0e
EJ Ciramella <eciramella@gmail.com>
2011-12-29 13:02:09 -0500

    Correcting the cleanup script to be more minimalistic

--- Maintenance/workspace-cleaner.groovy
+++ Maintenance/workspace-cleaner.groovy
@@ -2,9 +2,6 @@ import hudson.FilePath;
 
 for (slave in hudson.model.Hudson.instance.slaves)
 { 
-  println('Name: ' + slave.name); 
-  println('getRemoteFS: ' + slave.getRemoteFS()); 
-  println('getRootPath: ' + slave.getRootPath()); 
   FilePath fp = slave.createPath(slave.getRootPath().toString() + File.separator + "workspace"); 
   fp.deleteRecursive(); 
 }
